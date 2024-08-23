111dab5d9982213d95e56ece4c4f355c57757fb5
cirobt <cbtorres89@gmail.com>
2024-08-22 23:00:08 -0300

    Tags

---
+++ src/components/Tags/index.tsx
@@ -0,0 +1,205 @@
+import Link from "next/link";
+
+export default function Tags() {
+  return (
+    <>
+      <h4 className="uppercase font-extrabold text-base-neutral dark:text-dark-base-neutral after:content-[':']">
+        Categorias
+      </h4>
+      <ul className="flex flex-wrap items-center">
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            NextJS
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Javascript
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            HTML
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            CSS
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Python
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Java
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Análise de Algoritmo
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Sistemas Digitais
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            NextJS
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Javascript
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            HTML
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            CSS
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Python
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Java
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Análise de Algoritmo
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Sistemas Digitais
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            NextJS
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Javascript
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            HTML
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            CSS
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Python
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Java
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Análise de Algoritmo
+          </Link>
+        </li>
+        <li className="text-sm italic text-base-neutral dark:text-dark-base-neutral hover:text-base-100">
+          <Link
+            href="/"
+            className="leading-6 px-3 py-1 rounded hover:bg-base-blue dark:hover:bg-dark-base-blue-hover"
+          >
+            Sistemas Digitais
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+}
