9ba0a16f510bc89977539657ce1be0e822fcc778
cirobt <cbtorres89@gmail.com>
2024-09-03 22:42:44 -0300

    Height fixing

--- src/app/artigos/[topicSlug]/[id]/page.tsx
+++ src/app/artigos/[topicSlug]/[id]/page.tsx
@@ -28,8 +28,8 @@ export default async function ArticlePage({ params }: Params) {
     return (
       <main className="w-full h-full mx-0 bg-base-150 dark:bg-dark-base-150">
         <ArticleHero {...data} />
-        <div className="h-full mx-4 smartphone:mx-10 tablet:mx-20">
-          <div className="h-full w-full max-w-webpage mx-auto grid grid-cols-[minmax(300px,1fr)] tablet:gap-8 tablet:grid-cols-[minmax(180px,250px)_minmax(500px,1fr)] xl:gap-16 xl:grid-cols-[minmax(180px,250px)_minmax(500px,1fr)_75px]">
+        <div className="mx-4 smartphone:mx-10 tablet:mx-20">
+          <div className="w-full max-w-webpage mx-auto grid grid-cols-[minmax(300px,1fr)] tablet:gap-8 tablet:grid-cols-[minmax(180px,250px)_minmax(500px,1fr)] xl:gap-16 xl:grid-cols-[minmax(180px,250px)_minmax(500px,1fr)_75px]">
             <LinkAnchorTracker {...data} />
             <ArticleBody {...data} />
             <div className="self-start sticky top-1/2 -translate-y-1/2 mt-20 hidden xl:block">
